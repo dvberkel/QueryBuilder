fe5c18ad77f3fd4de0bf4d73309523b66f5dd273
dvberkel <daan.v.berkel.1980@gmail.com>
2011-04-01 18:01:14 +0200

    New SubExample testcase
    Subclasses should see their parent fields.

--- src/test/java/org/effrafax/querybuilder/reflection/ReflectionTest.java
+++ src/test/java/org/effrafax/querybuilder/reflection/ReflectionTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import java.lang.reflect.Field;
 
 import org.effrafax.querybuilder.test.Example;
+import org.effrafax.querybuilder.test.SubExample;
 import org.junit.Test;
 
 public class ReflectionTest
@@ -13,6 +14,16 @@ public class ReflectionTest
 	public void shouldReturnFields()
 	{
 		Class<?> aClass = Example.class;
+
+		Field[] fields = aClass.getFields();
+
+		assertTrue(2 == fields.length);
+	}
+
+	@Test
+	public void shouldReturnFieldsOfParent()
+	{
+		Class<?> aClass = SubExample.class;
 
 		Field[] fields = aClass.getFields();
 
