6c6d4b01fe6754f4765c92f5a4818d887a7ad0b7
dvberkel <daan.v.berkel.1980@gmail.com>
2011-04-10 15:14:11 +0200

    Introduced superclass
    An abstract superclass is introduced to handle the common case.

---
+++ src/main/java/org/effrafax/querybuilder/core/strategy/propertycriterium/PropertyCriteriumRepresentation.java
@@ -0,0 +1,29 @@
+package org.effrafax.querybuilder.core.strategy.propertycriterium;
+
+import org.effrafax.querybuilder.core.criteria.PropertyCriterium;
+
+import com.google.common.base.Function;
+
+public abstract class PropertyCriteriumRepresentation<T> implements Function<PropertyCriterium<T, ?>, String>
+{
+	@Override
+	public final String apply(PropertyCriterium<T, ?> propertyCriterium)
+	{
+		return representationOf(propertyCriterium);
+	}
+
+	private String representationOf(PropertyCriterium<T, ?> propertyCriterium)
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append(representationOfPropertyName(propertyCriterium));
+		builder.append(representationOfConnector());
+		builder.append(representationOfMatchValue(propertyCriterium));
+		return builder.toString();
+	}
+
+	protected abstract String representationOfPropertyName(PropertyCriterium<T, ?> propertyCriterium);
+
+	protected abstract String representationOfConnector();
+
+	protected abstract String representationOfMatchValue(PropertyCriterium<T, ?> propertyCriterium);
+}
