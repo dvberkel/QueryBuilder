d28b31861a4918fb428aec6ba2a3dc5b31c34a1c
dvberkel <daan.v.berkel.1980@gmail.com>
2011-04-10 15:06:11 +0200

    Introduced TopLevel class
    Same tune different words.

--- src/main/java/org/effrafax/querybuilder/core/strategy/LogStrategy.java
+++ src/main/java/org/effrafax/querybuilder/core/strategy/LogStrategy.java
@@ -6,9 +6,7 @@ import java.util.Collection;
 
 import org.apache.commons.lang.StringUtils;
 import org.effrafax.querybuilder.core.QueryBuilder;
-import org.effrafax.querybuilder.core.criteria.PropertyCriterium;
-
-import com.google.common.base.Function;
+import org.effrafax.querybuilder.core.strategy.propertycriterium.LogPropertyCriteriumRepresentation;
 
 public class LogStrategy implements Strategy
 {
@@ -30,39 +28,4 @@ public class LogStrategy implements Strategy
 			new LogPropertyCriteriumRepresentation<T>());
 		return StringUtils.join(representations, ", ");
 	}
-}
-
-class LogPropertyCriteriumRepresentation<T> implements Function<PropertyCriterium<T, ?>, String>
-{
-
-	@Override
-	public String apply(PropertyCriterium<T, ?> propertyCriterium)
-	{
-		return representationOf(propertyCriterium);
-	}
-
-	public String representationOf(PropertyCriterium<T, ?> propertyCriterium)
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append(representationOfPropertyName(propertyCriterium));
-		builder.append(representationOfConnector());
-		builder.append(representationOfMatchValue(propertyCriterium));
-		return builder.toString();
-	}
-
-	private String representationOfPropertyName(PropertyCriterium<T, ?> propertyCriterium)
-	{
-		return propertyCriterium.getPropertyName();
-	}
-
-	private String representationOfConnector()
-	{
-		return " = ";
-	}
-
-	private String representationOfMatchValue(PropertyCriterium<T, ?> propertyCriterium)
-	{
-		return propertyCriterium.getMatchValue().toString();
-	}
-
 }
