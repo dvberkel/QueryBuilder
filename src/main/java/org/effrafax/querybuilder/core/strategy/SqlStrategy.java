1669ac44294477f35e54ae85d5ad7a0b8721925f
dvberkel <daan.v.berkel.1980@gmail.com>
2011-04-10 15:02:46 +0200

    [REFACTOR] Toplevel class
    We upgraded a class to be a toplevel class.

--- src/main/java/org/effrafax/querybuilder/core/strategy/SqlStrategy.java
+++ src/main/java/org/effrafax/querybuilder/core/strategy/SqlStrategy.java
@@ -6,9 +6,7 @@ import java.util.Collection;
 
 import org.apache.commons.lang.StringUtils;
 import org.effrafax.querybuilder.core.QueryBuilder;
-import org.effrafax.querybuilder.core.criteria.PropertyCriterium;
-
-import com.google.common.base.Function;
+import org.effrafax.querybuilder.core.strategy.propertycriterium.SqlPropertyCriteriumRepresentation;
 
 public class SqlStrategy implements Strategy
 {
@@ -30,43 +28,4 @@ public class SqlStrategy implements Strategy
 			new SqlPropertyCriteriumRepresentation<T>());
 		return StringUtils.join(representations, " and ");
 	}
-}
-
-class SqlPropertyCriteriumRepresentation<T> implements Function<PropertyCriterium<T, ?>, String>
-{
-
-	@Override
-	public String apply(PropertyCriterium<T, ?> propertyCriterium)
-	{
-		return representationOf(propertyCriterium);
-	}
-
-	public String representationOf(PropertyCriterium<T, ?> propertyCriterium)
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append(representationOfPropertyName(propertyCriterium));
-		builder.append(representationOfConnector());
-		builder.append(representationOfMatchValue(propertyCriterium));
-		return builder.toString();
-	}
-
-	private String representationOfPropertyName(PropertyCriterium<T, ?> propertyCriterium)
-	{
-		return propertyCriterium.getPropertyName();
-	}
-
-	private String representationOfConnector()
-	{
-		return " = ";
-	}
-
-	private String representationOfMatchValue(PropertyCriterium<T, ?> propertyCriterium)
-	{
-		if (!(propertyCriterium.getMatchValue() instanceof Long))
-		{
-			return String.format("'%s'", propertyCriterium.getMatchValue());
-		}
-		return propertyCriterium.getMatchValue().toString();
-	}
-
 }
---
+++ src/main/java/org/effrafax/querybuilder/core/strategy/propertycriterium/SqlPropertyCriteriumRepresentation.java
@@ -0,0 +1,44 @@
+package org.effrafax.querybuilder.core.strategy.propertycriterium;
+
+import org.effrafax.querybuilder.core.criteria.PropertyCriterium;
+
+import com.google.common.base.Function;
+
+public class SqlPropertyCriteriumRepresentation<T> implements Function<PropertyCriterium<T, ?>, String>
+{
+
+	@Override
+	public String apply(PropertyCriterium<T, ?> propertyCriterium)
+	{
+		return representationOf(propertyCriterium);
+	}
+
+	public String representationOf(PropertyCriterium<T, ?> propertyCriterium)
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append(representationOfPropertyName(propertyCriterium));
+		builder.append(representationOfConnector());
+		builder.append(representationOfMatchValue(propertyCriterium));
+		return builder.toString();
+	}
+
+	private String representationOfPropertyName(PropertyCriterium<T, ?> propertyCriterium)
+	{
+		return propertyCriterium.getPropertyName();
+	}
+
+	private String representationOfConnector()
+	{
+		return " = ";
+	}
+
+	private String representationOfMatchValue(PropertyCriterium<T, ?> propertyCriterium)
+	{
+		if (!(propertyCriterium.getMatchValue() instanceof Long))
+		{
+			return String.format("'%s'", propertyCriterium.getMatchValue());
+		}
+		return propertyCriterium.getMatchValue().toString();
+	}
+
+}
