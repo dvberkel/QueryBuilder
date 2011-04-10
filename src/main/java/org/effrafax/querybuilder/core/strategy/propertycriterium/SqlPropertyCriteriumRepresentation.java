48c776325739721130ae5220c5f6d9fd2c8d260c
dvberkel <daan.v.berkel.1980@gmail.com>
2011-04-10 15:15:47 +0200

    Using Superclass
    the introduced superclass is also used for the log part.

--- src/main/java/org/effrafax/querybuilder/core/strategy/propertycriterium/SqlPropertyCriteriumRepresentation.java
+++ src/main/java/org/effrafax/querybuilder/core/strategy/propertycriterium/SqlPropertyCriteriumRepresentation.java
@@ -2,37 +2,23 @@ package org.effrafax.querybuilder.core.strategy.propertycriterium;
 
 import org.effrafax.querybuilder.core.criteria.PropertyCriterium;
 
-import com.google.common.base.Function;
-
-public class SqlPropertyCriteriumRepresentation<T> implements Function<PropertyCriterium<T, ?>, String>
+public class SqlPropertyCriteriumRepresentation<T> extends PropertyCriteriumRepresentation<T>
 {
 
 	@Override
-	public String apply(PropertyCriterium<T, ?> propertyCriterium)
-	{
-		return representationOf(propertyCriterium);
-	}
-
-	public String representationOf(PropertyCriterium<T, ?> propertyCriterium)
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append(representationOfPropertyName(propertyCriterium));
-		builder.append(representationOfConnector());
-		builder.append(representationOfMatchValue(propertyCriterium));
-		return builder.toString();
-	}
-
-	private String representationOfPropertyName(PropertyCriterium<T, ?> propertyCriterium)
+	protected String representationOfPropertyName(PropertyCriterium<T, ?> propertyCriterium)
 	{
 		return propertyCriterium.getPropertyName();
 	}
 
-	private String representationOfConnector()
+	@Override
+	protected String representationOfConnector()
 	{
 		return " = ";
 	}
 
-	private String representationOfMatchValue(PropertyCriterium<T, ?> propertyCriterium)
+	@Override
+	protected String representationOfMatchValue(PropertyCriterium<T, ?> propertyCriterium)
 	{
 		if (!(propertyCriterium.getMatchValue() instanceof Long))
 		{
